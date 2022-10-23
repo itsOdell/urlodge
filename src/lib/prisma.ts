d8786a04e79a94faeedc2c68a592f76ebf4345a2
abduladil sunnat <abduladilsunnat@gmail.com>
2022-10-23 09:45:55 -0500

    typo

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ declare global {
     var prisma: PrismaClient | undefined
   }
 
-const prism = global.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
