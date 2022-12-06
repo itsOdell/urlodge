093a701b0a4ed75fad564322544854593a1afb97
abduladil sunnat <abduladilsunnat@gmail.com>
2022-12-06 13:23:22 -0600

    hello api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,3 +1,9 @@
-export default async function handler(req:any, res: any) {
-    return res.send("hello")
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+ res.status(200).send("hello")
 }
+
